48e3805b052adb7f9db4c836c08d1c0886456e54
faketri <59346366+faketri@users.noreply.github.com>
2023-10-03 17:48:57 +0300

    add function getImageByProductId

--- src/main/java/com/faketri/market/contoller/ImageController.java
+++ src/main/java/com/faketri/market/contoller/ImageController.java
@@ -1,9 +1,12 @@
 package com.faketri.market.contoller;
 
+import com.faketri.market.entity.Image;
 import com.faketri.market.service.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ImageController {
@@ -14,5 +17,10 @@ public class ImageController {
     @RequestMapping(path = "/image/{idImage}", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
     public @ResponseBody byte[] getImageById(@PathVariable(value = "idImage") Long idImage){
         return imageService.findById(idImage).getPhoto();
+    }
+
+    @RequestMapping(path = "/image/product/{productId}", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
+    public @ResponseBody List<Image> getImageByProductId(@PathVariable(value = "productId") Long productId){
+        return imageService.findByProductId(productId);
     }
 }
